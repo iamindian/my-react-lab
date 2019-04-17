d3e1d5ad8f35f777425bc89a8ecacd34a3bd36a5
Zhimin Lin <zhimin.lin@fawdel.com>
2019-04-17 16:35:28 +0800

    add redux example code

--- src/react-redux-saga.js
+++ src/react-redux-saga.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, bindActionCreators } from 'redux'
+import { Provider, connect } from 'react-redux';
+function reducer(state, action) {
+    switch (action.type) {
+        case 'ADD':
+            return {
+                counter: state.counter + 1
+            }
+        case 'MIN':
+            return {
+                counter: state.counter - 1
+            }
+        default:
+            return {
+                counter: state.counter
+            };
+    }
+
+}
+const store = createStore(reducer, {
+    counter: 0
+});
+class Container extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        counter: state.counter
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({
+        increment: ()=> ({type:'ADD'}),
+        decrement: ()=>({type:'MIN'})
+    },dispatch);
+}
 
 class About extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return <div>About</div>
-	}
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return <div onClick={this.props.increment}>About:{this.props.counter}</div>
+    }
 }
-
-
 class Photo extends Component {
-	
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return <div>Photo</div>
-	}
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return <div onClick={this.props.decrement}>Photo:{this.props.counter}</div>
+    }
 }
-
-ReactDOM.render(<div><About></About><Photo></Photo></div>, document.getElementById('main'));
+const WrappedAbout = connect(mapStateToProps,mapDispatchToProps)(About);
+const WrappedPhoto = connect(mapStateToProps,mapDispatchToProps)(Photo);
+ReactDOM.render(<Provider store={store}><WrappedAbout></WrappedAbout><WrappedPhoto></WrappedPhoto></Provider>, document.getElementById('main'));
 
 
 
